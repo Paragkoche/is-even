11266c91cdaa9e239a2002c565fb2f9c34376508
Paragkoche <60149545+Paragkoche@users.noreply.github.com>
2022-08-02 16:24:27 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+function isEven(number) {
+    if (number === "even" || number === "Even" || number === "eVen" || number === "evEn" || number === "eveN" || number === "EVen" || number === "EvEn" || number === "EveN" || number === "eVEn" || number === "eVeN" || number == "evEN" || number === "eVEN" || number === "EvEN" || number === "EVeN" || number === "EVEn" || number === "EVEN") return true;
+}
+module.exports = isEven;
